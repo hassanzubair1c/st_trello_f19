0141f4e10a641d1beca868c554442e244eea47e2
Hassan XBOT <hassanxbot1c@gmail.com>
2019-11-17 18:55:32 +0500

    Test manage .java 8 tests added

--- test/trello/Team_ManagementNGTest.java
+++ test/trello/Team_ManagementNGTest.java
@@ -6,6 +6,8 @@
 package trello;
 
 import java.util.ArrayList;
+import java.util.Date;
+import org.testng.Assert;
 import static org.testng.Assert.*;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
@@ -18,455 +20,116 @@ import org.testng.annotations.Test;
  * @author Hassan X-BOT
  */
 public class Team_ManagementNGTest {
-    
+
+
+    private static Team_Management tmm;
+    private static Team team;
+    private static ArrayList<Team> tm;
+    private static List list;
+    private static Card card;
+    private static Date date=new Date();
+     private static User u1;
     public Team_ManagementNGTest() {
     }
 
     @BeforeClass
     public static void setUpClass() throws Exception {
+
+        tmm = new Team_Management();
+       // Team t1 = new Team("team1");
+        //Team t2 = new Team("team2");
+        tmm.addTeam("team1");
+        tmm.addTeam("team2");
+        tmm.addlist("team1", "list1");
+        tmm.addcard("team1", "list1", "card1");
+        tmm.addchecklist("team1", "list1", "card1", "checklist1");
+       
+        User u1 = new User("momin", "123456","momin.guner@gmail.com");
+        User u2 = new User("hassan", "123456","hassan.guner@gmail.com");
+        Card c1 = new Card("c1");
+        List l1 = new List("list1");
+         CheckList CL1 = new CheckList("clist1");
+        //xbot
+        card=new Card("c1",date,"testing");
+        card.addchecklisttocard("cl1");
+
+        
+        u1 = new User("momin", "123456","momin.guner@gmail.com");
+        card.addmemberTocard(u1.getName());
+        list=new List("myList");
+        list.addcard(card);
+       team=new Team("myTeam");
+       team.addlist(list);
+       
+       tm=new ArrayList<Team>();
+       tm.add(team);
+       
+        
     }
 
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
-
-    @BeforeMethod
-    public void setUpMethod() throws Exception {
-    }
-
-    @AfterMethod
-    public void tearDownMethod() throws Exception {
-    }
-
-    /**
-     * Test of getinstancetillassignedmembers method, of class Team_Management.
-     */
-    @Test
-    public void testGetinstancetillassignedmembers() {
-        System.out.println("getinstancetillassignedmembers");
-        Team_Management expResult = null;
-        Team_Management result = Team_Management.getinstancetillassignedmembers();
-        assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getinstancetillchecklist method, of class Team_Management.
-     */
-    @Test
-    public void testGetinstancetillchecklist() {
-        System.out.println("getinstancetillchecklist");
-        Team_Management expResult = null;
-        Team_Management result = Team_Management.getinstancetillchecklist();
-        assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getchecklist method, of class Team_Management.
-     */
     @Test
     public void testGetchecklist() {
         System.out.println("getchecklist");
-        String x = "";
-        String x2 = "";
-        String x3 = "";
-        Team_Management instance = new Team_Management();
-        ArrayList expResult = null;
-        ArrayList result = instance.getchecklist(x, x2, x3);
-        assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //System.out.println("Size: "+tmm.getchecklist("team1", "list1", "c1").size());
+        Assert.assertTrue(tmm.getchecklist("team1", "list1", "card").size()==0);
+        Assert.assertFalse(tmm.getchecklist("team1", "list1", "c1").size()==8);
     }
-
-    /**
-     * Test of getitems method, of class Team_Management.
-     */
+    
     @Test
     public void testGetitems() {
         System.out.println("getitems");
-        String x = "";
-        String x2 = "";
-        String x3 = "";
-        String x4 = "";
-        Team_Management instance = new Team_Management();
-        ArrayList expResult = null;
-        ArrayList result = instance.getitems(x, x2, x3, x4);
-        assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //System.out.println("Size: "+tmm.getitems("team1", "list1", "c1", "clist1").size()==0);
+        Assert.assertTrue(tmm.getitems("team1", "list1", "c1", "clist1").size()==0);
+        Assert.assertFalse(tmm.getitems("team1", "list1", "c1", "clist1").size()==8);
     }
-
-    /**
-     * Test of getinstancetillitems method, of class Team_Management.
-     */
-    @Test
-    public void testGetinstancetillitems() {
-        System.out.println("getinstancetillitems");
-        Team_Management expResult = null;
-        Team_Management result = Team_Management.getinstancetillitems();
-        assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getinstancetillcardnames method, of class Team_Management.
-     */
-    @Test
-    public void testGetinstancetillcardnames() {
-        System.out.println("getinstancetillcardnames");
-        Team_Management expResult = null;
-        Team_Management result = Team_Management.getinstancetillcardnames();
-        assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getinstancetillteamnames method, of class Team_Management.
-     */
-    @Test
-    public void testGetinstancetillteamnames() {
-        System.out.println("getinstancetillteamnames");
-        Team_Management expResult = null;
-        Team_Management result = Team_Management.getinstancetillteamnames();
-        assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getinstancetilllistnames method, of class Team_Management.
-     */
-    @Test
-    public void testGetinstancetilllistnames() {
-        System.out.println("getinstancetilllistnames");
-        Team_Management expResult = null;
-        Team_Management result = Team_Management.getinstancetilllistnames();
-        assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of addTeam method, of class Team_Management.
-     */
-    @Test
+    
+    @Test 
     public void testAddTeam() {
         System.out.println("addTeam");
-        String x = "";
-        Team_Management.addTeam(x);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        tmm.addTeam("test");
+        //System.out.println("Size: "+tmm.getTeam(1).getName());
+        Assert.assertTrue(tmm.getTeam(1).getName().equals("team2"));
+        Assert.assertFalse(tmm.getTeam(1).getName().equals("fail"));
     }
-
-    /**
-     * Test of addintoTeam method, of class Team_Management.
-     */
-    @Test
+    
+    @Test 
     public void testAddintoTeam() {
+        
         System.out.println("addintoTeam");
-        String x = "";
-        String y = "";
-        Team_Management instance = new Team_Management();
-        instance.addintoTeam(x, y);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+        tmm.addintoTeam("team1","jalal");
 
-    /**
-     * Test of getTeam method, of class Team_Management.
-     */
+        Assert.assertTrue(tmm.getTeam(0).getmembers().size()==1);
+        Assert.assertFalse(tmm.getTeam(0).getmembers().size()==0);
+    }
+    
     @Test
     public void testGetTeam() {
         System.out.println("getTeam");
-        int x = 0;
-        Team_Management instance = new Team_Management();
-        Team expResult = null;
-        Team result = instance.getTeam(x);
-        assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Assert.assertTrue(tmm.getTeam(0).getName().equals("team1") && tmm.getTeam(1).getName().equals("team2"));
+        Assert.assertFalse(tmm.getTeam(0).getName().equals("fail") && tmm.getTeam(1).getName().equals("fail"));
+
     }
 
-    /**
-     * Test of gettotalteams method, of class Team_Management.
-     */
     @Test
     public void testGettotalteams() {
         System.out.println("gettotalteams");
-        int expResult = 0;
-        int result = Team_Management.gettotalteams();
-        assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Assert.assertTrue(tmm.gettotalteams()==3);
+        Assert.assertFalse(tmm.gettotalteams()==0);
     }
-
-    /**
-     * Test of teamsjoined method, of class Team_Management.
-     */
+       
     @Test
     public void testTeamsjoined() {
-        System.out.println("teamsjoined");
-        String x = "";
-        Team_Management instance = new Team_Management();
-        int expResult = 0;
-        int result = instance.teamsjoined(x);
-        assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        System.out.println("teamsjoined : ");
+        Assert.assertTrue(tmm.teamsjoined("jalal")==1);
+        Assert.assertFalse(tmm.teamsjoined("fail")==6);
     }
-
-    /**
-     * Test of getmembers method, of class Team_Management.
-     */
+   
     @Test
     public void testGetmembers() {
-        System.out.println("getmembers");
-        String x = "";
-        Team_Management instance = new Team_Management();
-        ArrayList expResult = null;
-        ArrayList result = instance.getmembers(x);
-        assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of NamesListinteam method, of class Team_Management.
-     */
-    @Test
-    public void testNamesListinteam() {
-        System.out.println("NamesListinteam");
-        String x = "";
-        Team_Management instance = new Team_Management();
-        ArrayList expResult = null;
-        ArrayList result = instance.NamesListinteam(x);
-        assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of NamesCardinlistinteam method, of class Team_Management.
-     */
-    @Test
-    public void testNamesCardinlistinteam() {
-        System.out.println("NamesCardinlistinteam");
-        String x = "";
-        String x2 = "";
-        Team_Management instance = new Team_Management();
-        ArrayList expResult = null;
-        ArrayList result = instance.NamesCardinlistinteam(x, x2);
-        assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of membersincardassigned method, of class Team_Management.
-     */
-    @Test
-    public void testMembersincardassigned() {
-        System.out.println("membersincardassigned");
-        String x = "";
-        String x2 = "";
-        String x3 = "";
-        Team_Management instance = new Team_Management();
-        ArrayList expResult = null;
-        ArrayList result = instance.membersincardassigned(x, x2, x3);
-        assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of assignmember method, of class Team_Management.
-     */
-    @Test
-    public void testAssignmember() {
-        System.out.println("assignmember");
-        String x = "";
-        String x2 = "";
-        String x3 = "";
-        String x4 = "";
-        Team_Management instance = new Team_Management();
-        instance.assignmember(x, x2, x3, x4);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of ispresentinteam method, of class Team_Management.
-     */
-    @Test
-    public void testIspresentinteam() {
-        System.out.println("ispresentinteam");
-        String x = "";
-        String x2 = "";
-        Team_Management instance = new Team_Management();
-        boolean expResult = false;
-        boolean result = instance.ispresentinteam(x, x2);
-        assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of Namesteamsjoined method, of class Team_Management.
-     */
-    @Test
-    public void testNamesteamsjoined() {
-        System.out.println("Namesteamsjoined");
-        String x = "";
-        Team_Management instance = new Team_Management();
-        ArrayList expResult = null;
-        ArrayList result = instance.Namesteamsjoined(x);
-        assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of NamesCardsAssigned method, of class Team_Management.
-     */
-    @Test
-    public void testNamesCardsAssigned() {
-        System.out.println("NamesCardsAssigned");
-        String x = "";
-        Team_Management instance = new Team_Management();
-        ArrayList expResult = null;
-        ArrayList result = instance.NamesCardsAssigned(x);
-        assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getTeamsMade method, of class Team_Management.
-     */
-    @Test
-    public void testGetTeamsMade() {
-        System.out.println("getTeamsMade");
-        Team_Management instance = new Team_Management();
-        ArrayList expResult = null;
-        ArrayList result = instance.getTeamsMade();
-        assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of ispresentteam method, of class Team_Management.
-     */
-    @Test
-    public void testIspresentteam() {
-        System.out.println("ispresentteam");
-        String x = "";
-        Team_Management instance = new Team_Management();
-        boolean expResult = false;
-        boolean result = instance.ispresentteam(x);
-        assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of ispresentlist method, of class Team_Management.
-     */
-    @Test
-    public void testIspresentlist() {
-        System.out.println("ispresentlist");
-        String x = "";
-        String y = "";
-        Team_Management instance = new Team_Management();
-        boolean expResult = false;
-        boolean result = instance.ispresentlist(x, y);
-        assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of ispresentcard method, of class Team_Management.
-     */
-    @Test
-    public void testIspresentcard() {
-        System.out.println("ispresentcard");
-        String x = "";
-        String y = "";
-        String z = "";
-        Team_Management instance = new Team_Management();
-        boolean expResult = false;
-        boolean result = instance.ispresentcard(x, y, z);
-        assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of addlist method, of class Team_Management.
-     */
-    @Test
-    public void testAddlist() {
-        System.out.println("addlist");
-        String x = "";
-        String y = "";
-        Team_Management instance = new Team_Management();
-        instance.addlist(x, y);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of addcard method, of class Team_Management.
-     */
-    @Test
-    public void testAddcard() {
-        System.out.println("addcard");
-        String x = "";
-        String y = "";
-        String z = "";
-        Team_Management instance = new Team_Management();
-        instance.addcard(x, y, z);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of addchecklist method, of class Team_Management.
-     */
-    @Test
-    public void testAddchecklist() {
-        System.out.println("addchecklist");
-        String x = "";
-        String y = "";
-        String z = "";
-        String a = "";
-        Team_Management instance = new Team_Management();
-        instance.addchecklist(x, y, z, a);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of additem method, of class Team_Management.
-     */
-    @Test
-    public void testAdditem() {
-        System.out.println("additem");
-        String x = "";
-        String y = "";
-        String z = "";
-        String a = "";
-        String b = "";
-        Team_Management instance = new Team_Management();
-        instance.additem(x, y, z, a, b);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        System.out.println("getmembers: ");
+        
+        Assert.assertTrue(tmm.getmembers("team1").size()==1);
+        Assert.assertFalse(tmm.getmembers("team1").size()==5);
     }
     
 }
