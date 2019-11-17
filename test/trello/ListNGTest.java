0b65160b3eb05b072cddd8346f7cd013c9ea25bd
Hassan XBOT <hassanxbot1c@gmail.com>
2019-11-17 14:15:57 +0500

    list.java tests added

--- test/trello/ListNGTest.java
+++ test/trello/ListNGTest.java
@@ -6,6 +6,7 @@
 package trello;
 
 import java.util.ArrayList;
+import org.testng.Assert;
 import static org.testng.Assert.*;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
@@ -15,111 +16,57 @@ import org.testng.annotations.Test;
 
 /**
  *
- * @author Hassan X-BOT
+ * @author l134328
  */
 public class ListNGTest {
-    
+    private static List test_list ;
     public ListNGTest() {
     }
 
     @BeforeClass
     public static void setUpClass() throws Exception {
+        test_list=new List("ak");
+        Card c1 =new Card("test");
+        test_list.addcard(c1);
     }
 
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
+    
 
-    @BeforeMethod
-    public void setUpMethod() throws Exception {
-    }
-
-    @AfterMethod
-    public void tearDownMethod() throws Exception {
-    }
-
-    /**
-     * Test of isprescard method, of class List.
-     */
+    
     @Test
     public void testIsprescard() {
         System.out.println("isprescard");
-        String x = "";
-        List instance = null;
-        boolean expResult = false;
-        boolean result = instance.isprescard(x);
-        assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        Assert.assertTrue(test_list.isprescard("test"));
+        Assert.assertFalse(test_list.isprescard("fail"));
     }
 
-    /**
-     * Test of getname method, of class List.
-     */
-    @Test
+   
+    @Test 
     public void testGetname() {
         System.out.println("getname");
-        List instance = null;
-        String expResult = "";
-        String result = instance.getname();
-        assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Assert.assertTrue(test_list.getname().equals("ak"));
+        Assert.assertFalse(test_list.getname().equals("fail"));
     }
 
-    /**
-     * Test of getcards method, of class List.
-     */
-    @Test
+    
+    @Test (priority=0,dependsOnMethods = {"testGetname"}) 
     public void testGetcards() {
         System.out.println("getcards");
-        List instance = null;
-        ArrayList expResult = null;
-        ArrayList result = instance.getcards();
-        assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+       Assert.assertTrue(test_list.getcards().size()==1 && test_list.getcards().get(0).getname().equals("test"));
+       Assert.assertFalse(test_list.getcards().size()==2 && test_list.getcards().get(0).getname().equals("fail"));
     }
 
-    /**
-     * Test of addcard method, of class List.
-     */
-    @Test
+   
+    @Test (priority=1, dependsOnMethods = {"testGetname", "testGetcards"})
     public void testAddcard() {
         System.out.println("addcard");
-        Card c = null;
-        List instance = null;
-        instance.addcard(c);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Card cards= new Card("local");
+        test_list.addcard(cards);
+       Assert.assertTrue(test_list.getcards().size()==2 && test_list.getcards().get(1).getname().equals("local"));
+       Assert.assertFalse(test_list.getcards().size()==3 && test_list.getcards().get(1).getname().equals("fail"));
     }
 
-    /**
-     * Test of CreateCard method, of class List.
-     */
-    @Test
-    public void testCreateCard() {
-        System.out.println("CreateCard");
-        List instance = null;
-        boolean expResult = false;
-        boolean result = instance.CreateCard();
-        assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of MoveCard method, of class List.
-     */
-    @Test
-    public void testMoveCard() {
-        System.out.println("MoveCard");
-        List instance = null;
-        boolean expResult = false;
-        boolean result = instance.MoveCard();
-        assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+   
     
 }
